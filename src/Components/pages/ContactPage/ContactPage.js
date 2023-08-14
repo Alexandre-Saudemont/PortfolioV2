bdc0fba35f0d52fca3eed091e319c23748c8f83e
Tript1k <xetoxin@gmail.com>
2023-08-15 01:41:07 +0200

    setup api key for google map, setup emailJs, setup form for emil JS

--- src/Components/pages/ContactPage/ContactPage.js
+++ src/Components/pages/ContactPage/ContactPage.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import {getWeatherData} from '../../request';
 import {DarkModeContext} from '../../DarkMode/DarkModeContext/DarkModeContext';
 import {useNavigate} from 'react-router-dom';
+import {emailjs, sendForm} from '@emailjs/browser';
 import house from '../../../assets/img/house.svg';
 import houseDark from '../../../assets/img/house-dark.svg';
 import './ContactPage.scss';
@@ -16,6 +17,19 @@ function ContactPage() {
 	function navigateHome() {
 		navigate('/');
 	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		// Envoie l'e-mail via emailJS
+		sendForm('contact_service', 'template_l2nwp6s', event.target)
+			.then(() => {
+				alert('Message envoyé !');
+			})
+			.catch((error) => {
+				alert('Une erreur est survenue:', error);
+			});
+	};
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -36,6 +50,12 @@ function ContactPage() {
 			<img src={!isDarkMode ? house : houseDark} alt='icon house' className='contactPage-iconHome' onClick={navigateHome} />
 			{/* We make sure that weatherData is not null and after that we search the name of the city related to the lat & lon we setup above*/}
 			<p> {error && error.message}</p>
+			<form onSubmit={handleSubmit} className='contactPage-form'>
+				<input type='text' name='user_name' placeholder='Name' required className='contactPage-form-name' />
+				<input type='email' name='user_email' placeholder='E-mail' required className='contactPage-form-email' />
+				<textarea name='message' placeholder='Message' required className='contactPage-form-message'></textarea>
+				<input type='submit' value='Send' className='contactPage-form-button' />
+			</form>
 			<div className='contactPage-container-weather'>
 				<p className='contactPage-weather-city'>{weatherData && weatherData.name}</p>
 				<p className='contactPage-weather-description'>{weatherData && weatherData.weather[0].description}</p>
